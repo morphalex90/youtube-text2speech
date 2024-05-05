7203f337e350bc4e899162ab61b7aa99d7cf47ee
morphalex90 <piero.nanni@gmail.com>
2024-05-05 16:16:15 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# YouTube Downloader and speech to text
+
+## Install
+- `pip3 install -r requirements.txt --break-system-packages`
+- `bew install ffmpeg` if you are on MacOS, https://phoenixnap.com/kb/ffmpeg-windows if you are on Windows, `sudo apt install ffmpeg` if you are on Ubuntu
+
+## Run
+`python3 youtube.py`
---
+++ youtube.py
@@ -0,0 +1,15 @@
+filename = "output/yt_audio.mp3"
+
+# Download YouTube video as Audio ------------------------------------------
+import pytube as pt
+yt = pt.YouTube("https://www.youtube.com/shorts/J4HXVfi9FKw")
+stream = yt.streams.filter(only_audio=True)[0]
+stream.download(filename=filename)
+
+
+# Local Audio transcription ------------------------------------------
+import whisper
+model = whisper.load_model("base")
+result = model.transcribe(filename, fp16=False, language="en")
+transcription_text = result['text']
+print(transcription_text)
