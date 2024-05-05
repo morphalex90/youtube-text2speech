da0d1c6ffc02eb769a0bec83b48ed12b02c6c7f9
morphalex90 <piero.nanni@gmail.com>
2024-05-05 18:19:00 +0100

    import function to download and stt

--- search.py
+++ search.py
@@ -1,7 +1,11 @@
+from youtube import download_and_stt
+
+
 from youtube_search import YoutubeSearch
 
-results = YoutubeSearch('laravel', max_results=10).to_dict()
+results = YoutubeSearch('laravel', max_results=100).to_dict()
 # print(results)
 
 for x in results:
-  print( 'https://www.youtube.com/watch?v=' + x['id'] + ': ' + x['title'])
+    print( 'https://www.youtube.com/watch?v=' + x['id'] + ': ' + x['title'])
+    download_and_stt(x['id'])
--- youtube.py
+++ youtube.py
@@ -1,22 +1,29 @@
-filename = "output/yt_audio.mp3"
+# Download the YouTube video and speech to text
+def download_and_stt(youtube_url):
+    folder = "output/"
+    filename = folder + youtube_url
 
-# Download YouTube video as Audio ------------------------------------------
-import pytube as pt
-yt = pt.YouTube("https://www.youtube.com/shorts/J4HXVfi9FKw")
-stream = yt.streams.filter(only_audio=True)[0]
-stream.download(filename=filename)
-
-
-# Local Audio transcription ------------------------------------------
-import whisper
-model = whisper.load_model("base")
-result = model.transcribe(filename, fp16=False, language="en")
-transcription_text = result['text']
-print(transcription_text)
+    # Download YouTube video as Audio ------------------------------------------
+    import pytube as pt
+    yt = pt.YouTube("https://www.youtube.com/watch?v=" + youtube_url)
+    stream = yt.streams.filter(only_audio=True)[0]
+    stream.download(filename=filename+ '.mp3')
 
 
-# Generate audio from text ------------------------------------------
-from gtts import gTTS
+    # Local Audio transcription ------------------------------------------
+    import whisper
+    model = whisper.load_model("base")
+    result = model.transcribe(filename + '.mp3', fp16=False, language="en")
+    transcription_text = result['text']
+    print(transcription_text)
 
-myobj = gTTS(text=transcription_text, lang='en', tld='co.uk', slow=False)
-myobj.save("output/converted.mp3")
+    f = open(filename  + '.txt', "w")
+    f.write(transcription_text)
+    f.close()
+
+
+    # # Generate audio from text ------------------------------------------
+    # from gtts import gTTS
+
+    # myobj = gTTS(text=transcription_text, lang='en', tld='co.uk', slow=False)
+    # myobj.save("output/converted.mp3")
